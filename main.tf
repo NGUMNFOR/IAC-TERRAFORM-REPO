58c369db85ff6052a7ef639667e729891e3ae476
Ngum Jude Nfor <ngumjudenfor@Ngums-MacBook-Pro.local>
2022-06-13 14:00:27 -0400

    added

--- main.tf
+++ main.tf
@@ -1,40 +1,160 @@
 
-resource "aws_vpc" ngumvpc {
+
+locals {
+  mandatory_tags = {
+
+    line_of_business        = "hospital"
+    ado                     = "jude"
+    tier                    = "web"
+    operational_environment = upper(terraform.workspace)
+    tech_poc_primary        = "ngumjuden1234@gmail.com"
+    tech_poc_secondary      = "nngumjude@gmail.com"
+    application             = "http"
+    builder                 = "ngumjude@gmail.com"
+    application_ower        = "j.nfor@yahoo.co"
+    vpc                     = "WEB"
+    cell_name               = "WEB"
+    component_name          = "IAC-TERRAFORM-REPO"
+
+  }
+  vpc_id = aws_vpc.ngumvpc.id
+
+}
+
+resource "aws_vpc" "ngumvpc" {
   cidr_block           = "10.0.0.0/16"
   instance_tenancy     = "default"
   enable_dns_support   = true
   enable_dns_hostnames = true
+
+
+tags = {
+    Name = "ngumvpc"
 }
-resource "aws_subnet" pubsubnet1 {
-  vpc_id     = local.vpc_id
-  cidr_block = var.public_cidr[0]
- availability_zone = var.my_AZ[0]
- map_public_ip_on_launch = true
+
 }
-resource "aws_subnet" pubsubnet2 {
-  vpc_id     = local.vpc_id
-  cidr_block = var.public_cidr[1]
-  availability_zone = var.my_AZ[1]
+
+# internet gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = local.vpc_id
+
+  tags = {
+    Name = "ngumvpcigw"
+}
+}
+
+resource "aws_subnet" "pubsubnet1" {
+  vpc_id                  = local.vpc_id
+  cidr_block              = var.public_cidr[0]
+  availability_zone       = var.my_AZ[0]
   map_public_ip_on_launch = true
+
+  tags = {
+    Name = "pubsubnet1"
 }
-resource "aws_subnet" priv_sub1 {
-  vpc_id     = local.vpc_id
-  cidr_block = var.priv_cidr[0]
-  availability_zone = var.priv_AZ [0]
+
 }
-resource "aws_subnet" priv_sub2 {
-  vpc_id     = local.vpc_id
-  cidr_block = var.priv_cidr[1]
-  availability_zone = var.priv_AZ [1]
+
+
+resource "aws_subnet" "pubsubnet2" {
+  vpc_id                  = local.vpc_id
+  cidr_block              = var.public_cidr[1]
+  availability_zone       = var.my_AZ[1]
+  map_public_ip_on_launch = true
+
+  
+  tags = {
+    Name = "pubsubnet1"
 }
-resource "aws_subnet" database_subnet1 {
-  vpc_id     = local.vpc_id
-  cidr_block = var.cidr_database [0]
+
+}
+
+resource "aws_subnet" "priv_sub1" {
+  vpc_id            = local.vpc_id
+  cidr_block        = var.priv_cidr[0]
+  availability_zone = var.priv_AZ[0]
+}
+resource "aws_subnet" "priv_sub2" {
+  vpc_id            = local.vpc_id
+  cidr_block        = var.priv_cidr[1]
+  availability_zone = var.priv_AZ[1]
+}
+resource "aws_subnet" "database_subnet1" {
+  vpc_id            = local.vpc_id
+  cidr_block        = var.cidr_database[0]
   availability_zone = var.database_az[0]
 }
-resource "aws_subnet" database_subnet2 {
-  vpc_id     = local.vpc_id
-  cidr_block = var.cidr_database [1]
+resource "aws_subnet" "database_subnet2" {
+  vpc_id            = local.vpc_id
+  cidr_block        = var.cidr_database[1]
   availability_zone = var.database_az[1]
 }
 
+#Creating routs
+
+resource "aws_route_table" "pub_rt1" {
+  vpc_id = aws_vpc.ngumvpc.id
+
+}
+
+resource "aws_route_table" "pub_rt2" {
+  vpc_id = aws_vpc.ngumvpc.id
+
+}
+
+# Rout Table Association
+resource "aws_route_table_association" "rout_assiocia1" {
+  subnet_id      = aws_subnet.pubsubnet1.id
+  route_table_id = aws_route_table.pub_rt1.id
+}
+resource "aws_route_table_association" "rout_assiocia2" {
+  subnet_id      = aws_subnet.pubsubnet2.id
+  route_table_id = aws_route_table.pub_rt2.id
+}
+
+#Route
+
+resource "aws_route" "pub_rt1" {
+  route_table_id            = aws_route_table.pub_rt1.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id                = aws_internet_gateway.igw.id
+  depends_on                = [aws_route_table.pub_rt1]
+}
+
+resource "aws_route" "pub_rt2" {
+  route_table_id            = aws_route_table.pub_rt2.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id                = aws_internet_gateway.igw.id
+  depends_on                = [aws_route_table.pub_rt2]
+}
+
+#Default rout table
+
+resource "aws_default_route_table" "default_rout" {
+  default_route_table_id = aws_vpc.ngumvpc.default_route_table_id
+}
+  /*route {
+    cidr_block = "0.0.0.0/0"
+    nateway_id = aws_nat_gateway.ngw_1.id
+  }
+ */
+#Natgateway 
+
+resource "aws_nat_gateway" "ngw_1" {
+  allocation_id = aws_eip.eip.id
+  subnet_id     = aws_subnet.pubsubnet1.id
+ 
+  depends_on = [aws_internet_gateway.igw]
+}
+
+#Elastic IP address
+
+resource "aws_eip" "eip" {
+ depends_on  =  [aws_internet_gateway.igw]  
+  vpc      = true
+}
+
+ /*tags = {
+    Name = "gw NAT"
+  }
+*/
--- variable.tf
+++ variable.tf
@@ -1,41 +1,41 @@
 
-variable aws_region {
+variable "aws_region" {
   type        = string
   description = "This is for my region"
   default     = "us-east-1"
 }
-variable public_cidr {
-    type = list
-    description = " list of  public cidr"
-    default = ["10.0.0.0/24", "10.0.2.0/24"]
+variable "public_cidr" {
+  type        = list(any)
+  description = " list of  public cidr"
+  default     = ["10.0.0.0/24", "10.0.2.0/24"]
 }
 
-variable priv_cidr {
-    type = list
-    description = "list of  private cidr"
-     default =  ["10.0.1.0/24", "10.0.3.0/24"]
+variable "priv_cidr" {
+  type        = list(any)
+  description = "list of  private cidr"
+  default     = ["10.0.1.0/24", "10.0.3.0/24"]
 
 }
-variable cidr_database {
-    type = list
-    description = "list of  cidr database"
-    default = ["10.0.5.0/24","10.0.7.0/24"]
+variable "cidr_database" {
+  type        = list(any)
+  description = "list of  cidr database"
+  default     = ["10.0.5.0/24", "10.0.7.0/24"]
 }
 
-  variable my_AZ {
-    type = list
-    description = "list of my public AZ"
-    default = ["us-east-1a", "us-east-1b"]
+variable "my_AZ" {
+  type        = list(any)
+  description = "list of my public AZ"
+  default     = ["us-east-1a", "us-east-1b"]
 }
-  
-  variable priv_AZ {
-        type = list
-        description = "list of azs"
-        default = ["us-east-1a", "us-east-1b"]
+
+variable "priv_AZ" {
+  type        = list(any)
+  description = "list of azs"
+  default     = ["us-east-1a", "us-east-1b"]
 }
-   variable database_az {
-        type = list
-        description =  " list of public database"
-        default = ["us-east-1a", "us-east-1b"]
-    }
+variable "database_az" {
+  type        = list(any)
+  description = " list of public database"
+  default     = ["us-east-1a", "us-east-1b"]
+}
 
